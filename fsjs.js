155ccb4730cbaf78f44b0a67fa47917cda2e8a4e
Arpith Siromoney <arpith@shelf.name>
2013-04-11 12:01:29 +0530

    Changed scope of urlparts, method and callback

--- fsjs.js
+++ fsjs.js
@@ -17,21 +17,22 @@ var http = require('http')
   })
 }
 exports = function(port){
-  var callback = function(data){
-    if (typeof data === 'string') res.write(data)
-    else {
-      res.writeHead(200,{"Content-Type":"application/json"})
-      res.write(JSON.stringify(data))
-    }
-    res.end()
-  }
+  var callback = null
   , dir = path.dirname(require.main.filename)
-  , urlparts = url.parse(req.url,true).pathname.split('/')
-  , method = req.method.toLowerCase()
   if (typeof arguments[arguments.length-1]==='function') callback = Array.prototype.slice.call(arguments).pop()
   requireAll(dir,function(){
     fs.watch(dir,requireAgain)
     http.createServer(function(req,res){
+      var urlparts = url.parse(req.url,true).pathname.split('/')
+      , method = req.method.toLowerCase()
+      if (!callback) callback = function(data){
+        if (typeof data === 'string') res.write(data)
+        else {
+          res.writeHead(200,{"Content-Type":"application/json"})
+          res.write(JSON.stringify(data))
+        }
+        res.end()
+      }
       if (((name=urlparts[0]))&&(require.cache[name])) require.cache[name].method.apply(this,urlparts.slice(1),callback)
       else require.main.exports.method.apply(this,urlparts,callback)
     }).listen(port)
