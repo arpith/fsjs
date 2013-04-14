aa26fe4531569cb76cb254f46244a9406f4db2f9
Arpith Siromoney <arpith@feedreader.co>
2013-04-14 11:54:36 +0000

    dont cycle require

--- fsjs.js
+++ fsjs.js
@@ -4,26 +4,26 @@ var http = require('http')
 , url = require('url')
 , dir = path.dirname(require.main.filename)
 , requireAgain = function(event,filename){
-  var ext = path.extname(filename)
-  , name = path.basename(filename,ext)
+  var name = path.resolve(dir,filename)
   if (require.cache[name]) delete require.cache[name]
-  require(path.resolve(dir,filename))
+  require(name)
 }
 , requireAll = function(callback){
   fs.readdir(dir,function(e,files){
     if (!e) files.forEach(function(filename,filenumber){
-      requireAgain('rename',filename)
+      var name = path.resolve(dir,filename)
+      console.log(name)
+      if (!require.cache[name]) require(name)
       if (filenumber===files.length-1) callback()
     })
   })
 }
 module.exports = function(port){
   var args = Array.prototype.slice.call(arguments)
+  fs.watch(dir,requireAgain)
   requireAll(function(){
-    fs.watch(dir,requireAgain)
     http.createServer(function(req,res){
-      var urlpathname = url.parse(req.url).pathname
-      , urlparts = urlpathname.split('/').slice(1)
+      var urlparts = url.parse(req.url).pathname.split('/').slice(1)
       , method = req.method.toLowerCase()
       , callback = function(data){
         if (typeof data === 'string') res.write(data)
@@ -37,12 +37,12 @@ module.exports = function(port){
         var newCallback = args.pop
         newCallback(data,callback)
       }
+      if (urlparts[urlparts.length-1]==="") urlparts.pop()
       args.slice(1).forEach(function(arg){
         if (!urlparts.length) return
         if (require.cache[path.resolve(dir,urlparts[0]+".js")]) return
         this[arg] = urlparts.shift()
       })
-      if (urlparts[urlparts.length-1]==="") urlparts.pop()
       if ((modu=require.cache[path.resolve(dir,urlparts[0]+'.js')])) {
         modu.exports[method].apply(this,urlparts.slice(1).concat([callback]))
       }
