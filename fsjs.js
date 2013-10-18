1d3000dd61d6b42cce7fca1af4f472cb9a8374cd
Arpith Siromoney <arpith@feedreader.co>
2013-10-18 18:03:09 +0530

    watchDirectory doesn't need callback

--- fsjs.js
+++ fsjs.js
@@ -31,45 +31,43 @@ var http = require('http')
     }
   })
 }
-, watchDirectory = function(callback){
+, watchDirectory = function(){
   fs.watch(dir,function(event,filename){
     requireFile(filename)
   })
-  callback()
 }
 module.exports = function(port){
   var args = Array.prototype.slice.call(arguments)
   requireDirectory(function(){
-    watchDirectory(function(){
-      http.createServer(function(req,res){
-        var urlparts = url.parse(req.url).pathname.split('/').slice(1)
-        , method = req.method.toLowerCase()
-        , defaultCallback = function(data){
-          if (typeof data === 'string') res.write(data)
-          else {
-            res.writeHead(200,{"Content-Type":"application/json"})
-            res.write(JSON.stringify(data))
-          }
-          res.end()
+    watchDirectory()
+    http.createServer(function(req,res){
+      var urlparts = url.parse(req.url).pathname.split('/').slice(1)
+      , method = req.method.toLowerCase()
+      , defaultCallback = function(data){
+        if (typeof data === 'string') res.write(data)
+        else {
+          res.writeHead(200,{"Content-Type":"application/json"})
+          res.write(JSON.stringify(data))
         }
-        if (typeof args[args.length-1]==='function') callback = function(data){
-          args[args.length-1](data,defaultCallback)
-        }
-        else callback = defaultCallback
-        if (urlparts[urlparts.length-1]==="") urlparts.pop()
-        args.slice(1).forEach(function(arg){
-          if (!urlparts.length) return
-          if (require.cache[path.resolve(dir,urlparts[0]+".js")]) return
-          if (typeof arg !== "string") return
-          this[arg] = urlparts.shift()
-        })
-        this.request = req
-        this.response = res
-        if ((modu=require.cache[path.resolve(dir,urlparts[0]+'.js')])) {
-          modu.exports[method].apply(this,urlparts.slice(1).concat([callback]))
-        }
-        else require.main.exports[method].apply(this,urlparts.concat([callback]))
-      }).listen(port)
-    })
+        res.end()
+      }
+      if (typeof args[args.length-1]==='function') callback = function(data){
+        args[args.length-1](data,defaultCallback)
+      }
+      else callback = defaultCallback
+      if (urlparts[urlparts.length-1]==="") urlparts.pop()
+      args.slice(1).forEach(function(arg){
+        if (!urlparts.length) return
+        if (require.cache[path.resolve(dir,urlparts[0]+".js")]) return
+        if (typeof arg !== "string") return
+        this[arg] = urlparts.shift()
+      })
+      this.request = req
+      this.response = res
+      if ((modu=require.cache[path.resolve(dir,urlparts[0]+'.js')])) {
+        modu.exports[method].apply(this,urlparts.slice(1).concat([callback]))
+      }
+      else require.main.exports[method].apply(this,urlparts.concat([callback]))
+    }).listen(port)
   })
 }
