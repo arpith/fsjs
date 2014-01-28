25398123a9028c078e66505b09021c1a207600ff
Arpith Siromoney <arpith@feedreader.co>
2014-01-28 10:39:49 +0530

    Seem to have forgotten how callbacks work

--- fsjs.js
+++ fsjs.js
@@ -32,39 +32,41 @@ var http = require('http')
 }
 module.exports = function(port){
   var args = Array.prototype.slice.call(arguments)
-  watchDir(requireDir(function(){
-    http.createServer(function(req,res){
-      var urlparts = url.parse(req.url).pathname.split('/').slice(1)
-      , method = req.method.toLowerCase()
-      , defaultCallback = function(data){
-        if (typeof data === 'string') res.write(data)
-        else {
-          res.writeHead(200,{'Content-Type':'application/json'})
-          res.write(JSON.stringify(data))
+  watchDir(function(){
+    requireDir(function(){
+      http.createServer(function(req,res){
+        var urlparts = url.parse(req.url).pathname.split('/').slice(1)
+        , method = req.method.toLowerCase()
+        , defaultCallback = function(data){
+          if (typeof data === 'string') res.write(data)
+          else {
+            res.writeHead(200,{'Content-Type':'application/json'})
+            res.write(JSON.stringify(data))
+          }
+          res.end()
         }
-        res.end()
-      }
-      if (typeof args[args.length-1]==='function') callback = function(data){
-        args[args.length-1](data,defaultCallback)
-      }
-      else callback = defaultCallback
-      if (urlparts[urlparts.length-1]==='') urlparts.pop()
-      args.slice(1).forEach(function(arg){
-        if (!urlparts.length) return
-        if (require.cache[path.resolve(dir,urlparts[0]+'.js')]) return
-        if (typeof arg !== 'string') return
-        this[arg] = urlparts.shift()
+        if (typeof args[args.length-1]==='function') callback = function(data){
+          args[args.length-1](data,defaultCallback)
+        }
+        else callback = defaultCallback
+        if (urlparts[urlparts.length-1]==='') urlparts.pop()
+        args.slice(1).forEach(function(arg){
+          if (!urlparts.length) return
+          if (require.cache[path.resolve(dir,urlparts[0]+'.js')]) return
+          if (typeof arg !== 'string') return
+          this[arg] = urlparts.shift()
+        })
+        this.request = req
+        this.response = res
+        var modu = require.cache[path.resolve(dir,urlparts[0]+'.js')]
+        if (modu && modu.exports[method]) {
+          modu.exports[method].apply(this,urlparts.slice(1).concat([callback]))
+        }
+        else if (require.main.exports[method]) {
+          require.main.exports[method].apply(this,urlparts.concat([callback]))
+        }
+        else callback('oops')
       })
-      this.request = req
-      this.response = res
-      var modu = require.cache[path.resolve(dir,urlparts[0]+'.js')]
-      if (modu && modu.exports[method]) {
-        modu.exports[method].apply(this,urlparts.slice(1).concat([callback]))
-      }
-      else if (require.main.exports[method]) {
-	    require.main.exports[method].apply(this,urlparts.concat([callback]))
-      }
-      else callback('oops')
     }).listen(port)
   })
 }
